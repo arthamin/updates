d26b053fd1bf271f5b6e9d67d435ed00f43dca4f
ArthurDelux0 <deluxo1972@protonmail.com>
2023-04-29 12:21:08 +0500

    Template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PB push_back
+#define V vector <int>
+#define find in
+
+void print(vector <int> a)
+{
+    for(auto i : a) {cout << i << " ";}
+    cout << "\n";
+}
+
+
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+}
