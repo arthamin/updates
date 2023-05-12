dc7accd034b72b72a943e194487dfb3d6b30c384
ArthurDelux0 <deluxo1972@protonmail.com>
2023-05-12 22:28:42 +0500

    template.cpp

--- template.cpp
+++ template.cpp
@@ -1,17 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 #define ll long long
 #define PB push_back
-#define V vector <int>
-
-
-void print(vector <int> a)
-{
-    for(auto i : a) {cout << i << " ";}
-    cout << "\n";
-}
-
 
 
 
