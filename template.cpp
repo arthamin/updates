e970a093b5f09fb9bd91668509d5de8c80f78804
ArthurDelux0 <deluxo1972@protonmail.com>
2023-06-10 21:43:09 +0500

    new commit

--- template.cpp
+++ template.cpp
@@ -2,14 +2,48 @@
 using namespace std;
 
 
-#define ll long long
 #define PB push_back
+#define vt vector
+#define mset multiset
+#define mmap multimap
+#define V vector
+#define speed ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+
+const ld pi = 3.141592653589793;
+
+
+void maxi(ll a, ll b, ll c)
+{
+    cout << max(c, max(a,b)) << "\n";
+}
+
+
+int gcd(ll a, ll b)
+{
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
 
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    speed;
+    cout << "\n";
 
-}
+
+
+   // ##Genius solution##
+
+
+
+    cout << "\n";
+
+    return 0;
+
+} 
