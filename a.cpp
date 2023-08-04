71ded47d3221e453668c465e8ec8e8e65a5c951b
ArthurDelux0 <deluxo1972@protonmail.com>
2023-08-04 18:22:12 +0500

    template

--- a.cpp
+++ a.cpp
@@ -6,14 +6,16 @@ using namespace std;
   
 
 #define PB push_back
-#define vt vector
-#define mset multiset
-#define mmap multimap
-#define V vector
-#define bg begin()
-#define end end()
+#define PP pop_back
 #define sub substr
-#define len size
+#define V vector
+#define F first
+#define S second
+#define ins insert
+#define setprec(x,y) setprecision(x) << fixed << y
+#define len(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define make_unique(x) sort(all(x)), x.erase(unique(all(x)), x.end())
 #define speed ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
  
  
@@ -21,6 +23,18 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 typedef string str;
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef vector <pair<int,int>> vip;
+typedef vector <pair<ll,ll>> vill;
+typedef set <int> seti;
+typedef set <ll> setll;
+typedef map <int,int> mapi;
+typedef map <ll,ll> mapll;
+typedef multiset <int,int> mset;
+typedef set <int> seti;
+typedef set <ll> setll;
+typedef vector <long double> vld;
  
  
 const ld pi = 3.141592653589793;
@@ -52,30 +66,49 @@ int countSubs(const std::string& str, const std::string& sub)
 }
 
 
-int sum(V <int> &a)
+int sum(vi &a)
 {
-    int s = 0;for(int i = 0; i < (int)a.len();i++) {s += a[i];} return s;
+    int s = 0;for(int i = 0; i < len(a);i++) {s += a[i];} return s;
 }
 
 
-int prod(V <int> &a)
+int prod(vi &a)
 {
-    int p = 1;for(int i = 0; i < (int)a.len(); i++) {p *= a[i];} return p;
+    int p = 1;for(int i = 0; i < len(a); i++) {p *= a[i];} return p;
 }
 
 
-int in(V <int> &a,int x)
+int in(vi &a,int x)
 {
-    for(int i = 0; i < (int)a.size(); i++) {if(a[i] == x) return i;} return -1;
+    for(int i = 0; i < len(a); i++) {if(a[i] == x) return i;} return -1;
 }
 
-  
+
+void solve()
+{
+
+
+    
+
+    
+
+
+}
+
+
 int main()
 {
     speed;
 
-    
+    int t;
+    cin >> t;
 
+
+    while(t--) {
+        solve();
+    }
+
+    
 
     return 0;
 }
